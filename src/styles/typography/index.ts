35714ad51aa8440faeac9134115794d2eff224ac
Lucas <bellierlucas.pro@gmail.com>
2024-03-11 23:46:34 +0100

    ✨ Add typography styles for mega large and title section text

--- src/styles/typography/index.ts
+++ src/styles/typography/index.ts
@@ -1,10 +1,40 @@
 import styled from "styled-components";
 
+export const TextMegaLarge = styled.h1`
+  color: ${({ theme }) => theme.colors.text.contrast};
+  text-align: center;
+  font-family: "Roslindale Display";
+  font-size: 96px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  margin-bottom: ${({ theme }) => theme.spacing.xl};
+  margin-top: ${({ theme }) => theme.spacing.md};
+`;
+
+export const TextTitleSection = styled.h2`
+  color: ${({ theme }) => theme.colors.text.contrast};
+  text-align: center;
+  font-family: "Roslindale Display";
+  font-size: 88px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    font-size: 72px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    font-size: 40px;
+  }
+`;
+
 export const TextBasic = styled.p`
   color: ${({ theme }) => theme.colors.text.contrast};
   font-family: "Figtree";
   font-size: 16px;
   font-style: normal;
-  font-weight: 400;
+  font-weight: 300;
   line-height: normal;
 `;
