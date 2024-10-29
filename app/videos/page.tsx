dedda99c63749ee20ed47115b459f28225e952b5
ImJustLucas <lucas.bellier@icloud.com>
2024-10-29 14:22:16 +0100

    💬 add text on videos page

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,9 +1,15 @@
-import { TypographyH2 } from "@/components/typography";
+import { TypographyH2, TypographyP } from "@/components/typography";
 
 export default function What() {
   return (
-    <div className="">
-      <TypographyH2 border={false}>yt videos</TypographyH2>
+    <div className="w-2/3">
+      <TypographyH2 border={false}>youtube videos</TypographyH2>
+      <TypographyP>
+        This section is a bit empty right now, i know. I&apos;m planning to
+        start making videos about software development, lifestyle, life in
+        general and other things that i find interesting. Keep an eye on this
+        page 🤓
+      </TypographyP>
     </div>
   );
 }
