8e7572658f4dc4fa144c5ad329dc5ee8349532ac
ImJustLucas <lucas.bellier@icloud.com>
2025-01-04 21:40:30 +0100

    ✨ Update video titles and add new video entries

--- app/videos/config/videos.ts
+++ app/videos/config/videos.ts
@@ -9,7 +9,7 @@ export interface YouTubeVideo {
 export const videos: YouTubeVideo[] = [
   {
     id: "1",
-    title: "Une journée avec un développeur web étudiant | vlog dev",
+    title: "EP01: Une journée dans la vie d'un développeur web étudiant",
     thumbnail: "/images/videos/Video1.png",
     url: "https://youtu.be/xzs5jgJWWVU",
     publishedAt: "24-11-2024",
@@ -18,8 +18,26 @@ export const videos: YouTubeVideo[] = [
   {
     id: "2",
     title:
-      "On upgrade (un peu) mon setup | Une journée en télétravail d’un alternant",
+      "EP02: Une journée dans la vie d'un développeur web étudiant | On upgrade (un peu) mon setup",
     thumbnail: "/images/videos/Video2.png",
+    url: "https://youtu.be/xpul90E3VXM?si=kiqaWnQCWnTRpoH8",
+    publishedAt: "8-12-2024",
+    new: false,
+  },
+  {
+    id: "3",
+    title:
+      "Comment devenir développeur web ? Mes études et mon parcours d'étudiant",
+    thumbnail: "/images/videos/Video3.png",
+    url: "https://youtu.be/xpul90E3VXM?si=kiqaWnQCWnTRpoH8",
+    publishedAt: "8-12-2024",
+    new: false,
+  },
+  {
+    id: "4",
+    title:
+      "EP03: Une journée dans la vie d'un développeur web étudiant | Mes objectif pour 2025",
+    thumbnail: "/images/videos/Video4.png",
     url: "https://youtu.be/xpul90E3VXM?si=kiqaWnQCWnTRpoH8",
     publishedAt: "8-12-2024",
     new: true,
